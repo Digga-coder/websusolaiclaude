bdb0d018647f882ab5ed8fa445e06ddd8e2e77f8
Digga-coder <diegogar11@gmail.com>
2025-06-14 18:35:16 +0200

    Comment auto-click and log button status

--- cypress/e2e/modal.cy.js
+++ cypress/e2e/modal.cy.js
@@ -1,6 +1,7 @@
 describe('project modal', () => {
   it('opens project details modal', () => {
     cy.visit('/portafolio.html');
+    cy.injectAxe();
     cy.checkA11y();
     cy.get('.view-details-btn').first().click();
     cy.get('#projectModal').should('have.css', 'display', 'flex');
